cb5f5727d10e49ff509c4fe8bc2a85bd15634815
Tim VanFosson <tvanfosson@gmail.com>
2013-08-30 15:20:50 -0500

    Document public method of hash algorithm.

--- BloomFilter/SpellChecker/SpellChecker/FNV1aHashAlgorithm.cs
+++ BloomFilter/SpellChecker/SpellChecker/FNV1aHashAlgorithm.cs
@@ -11,6 +11,11 @@ namespace SpellChecker
         private const uint FnvBasis = 2166136261U;
         private const uint FnvPrime = 16777619U;
 
+        /// <summary>
+        /// Computes the FNV1a hash of the collection of bytes.
+        /// </summary>
+        /// <param name="bytes">A collection of bytes to hash.</param>
+        /// <returns>The FNV1a hash value.</returns>
         public uint ComputeHash(IEnumerable<byte> bytes)
         {
             unchecked
