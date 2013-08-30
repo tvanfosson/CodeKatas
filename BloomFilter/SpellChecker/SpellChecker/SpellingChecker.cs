e8f28492d96e7a14f46817c1242e237e072f4dc6
Tim VanFosson <tvanfosson@gmail.com>
2013-08-30 15:19:15 -0500

    Organize private methods and annotate public methods.

--- BloomFilter/SpellChecker/SpellChecker/SpellingChecker.cs
+++ BloomFilter/SpellChecker/SpellChecker/SpellingChecker.cs
@@ -15,7 +15,7 @@ namespace SpellChecker
 
         /// <summary>
         ///     Create a spelling checker large enough to handle the given number of words,
-        ///     using the specified number of hashes.
+        ///     using the specified number of hashes. Implemented using a Bloom filter.
         /// </summary>
         /// <param name="hashAlgorithm"></param>
         /// <param name="wordCount"></param>
@@ -27,28 +27,6 @@ namespace SpellChecker
             _hashCount = hashCount;
 
             _hashAlgorithm = hashAlgorithm;
-        }
-
-        public void Add(string word)
-        {
-            for (var i = 0; i < _hashCount; ++i)
-            {
-                _bits.Set(ComputeIndex(word, i), true);
-            }
-        }
-
-        public bool Check(string word)
-        {
-            for (var i = 0; i < _hashCount; ++i)
-            {
-                var bit = ComputeIndex(word, i);
-                if (!_bits[bit])
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
 
         private static int ComputeBitArraySize(int wordCount, int hashCount)
@@ -67,6 +45,43 @@ namespace SpellChecker
 
                 return (int)(hash % _bits.Count);
             }
+        }
+
+        /// <summary>
+        /// Add a word to the dictionary.
+        /// </summary>
+        /// <param name="word">The word to be added.</param>
+        public void Add(string word)
+        {
+            for (var i = 0; i < _hashCount; ++i)
+            {
+                _bits.Set(ComputeIndex(word, i), true);
+            }
+        }
+
+        /// <summary>
+        /// Check if a word is in the dictionary. There is
+        /// a small possibility that a word not in the
+        /// dictionary may result in a positive response
+        /// if the hashed indexes collide with those
+        /// of a different word or set of words in
+        /// the dictionary.
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns>False, if the word is not in the dictionary. True, if the word
+        /// is in the dictionary or there was a collision with existing hashes.</returns>
+        public bool Check(string word)
+        {
+            for (var i = 0; i < _hashCount; ++i)
+            {
+                var bit = ComputeIndex(word, i);
+                if (!_bits[bit])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
