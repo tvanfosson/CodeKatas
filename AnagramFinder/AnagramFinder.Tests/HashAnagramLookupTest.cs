4f4d8a51970b49789dc3d9c2fe2a1c4b12299ec7
Tim VanFosson <tvanfosson@gmail.com>
2013-12-14 12:49:47 -0600

    Add null argument checking for word

---
+++ AnagramFinder/AnagramFinder.Tests/HashAnagramLookupTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramFinder.Tests
+{
+    [TestClass]
+    public class HashAnagramLookupTest
+    {
+        private HashAnagramLookupTestContext _c;
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void When_a_null_word_is_given_an_argument_null_exception_is_thrown()
+        {
+            var algorithm = _c.GetAlgorithm();
+
+            algorithm.FindAnagrams(null, new[] { "word" });
+        }
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            _c = new HashAnagramLookupTestContext();
+        }
+
+        private class HashAnagramLookupTestContext
+        {
+            public IAnagramLookup GetAlgorithm()
+            {
+                return new HashAnagramLookup();
+            }
+        }
+    }
+}
--- AnagramFinder/AnagramFinder/HashAnagramLookup.cs
+++ AnagramFinder/AnagramFinder/HashAnagramLookup.cs
@@ -7,6 +7,11 @@ namespace AnagramFinder
     {
         public IEnumerable<string> FindAnagrams(string word, IEnumerable<string> wordList)
         {
+            if (string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentNullException("word");
+            }
+
             throw new NotImplementedException();
         }
     }
