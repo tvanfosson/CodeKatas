60e7d2780672d2e74fa588747f195a8c56467164
Tim VanFosson <tvanfosson@gmail.com>
2013-12-14 12:35:18 -0600

    Add stub implementation of a hash-based anagram lookup implementation

---
+++ AnagramFinder/AnagramFinder/HashAnagramLookup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramFinder
+{
+    public class HashAnagramLookup : IAnagramLookup
+    {
+        public IEnumerable<string> FindAnagrams(string word, IEnumerable<string> wordList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
