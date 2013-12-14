acd716004ddb07fa73afafd510a6490f709c47f4
Tim VanFosson <tvanfosson@gmail.com>
2013-12-14 13:50:53 -0600

    Add word list test helper class.

---
+++ AnagramFinder/AnagramFinder.Tests/WordListHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AnagramFinder.Tests
+{
+    public static class WordListHelper
+    {
+        public static List<string> GetAnagramList()
+        {
+            return new List<string>
+                {
+                    "anestri",
+                    "asterin",
+                    "eranist",
+                    "nastier",
+                    "ratines",
+                    "resiant",
+                    "restain",
+                    "retains",
+                    "retinas",
+                    "retsina",
+                    "sainter",
+                    "stainer",
+                    "starnie",
+                    "stearin"
+                };
+        } 
+    }
+}
