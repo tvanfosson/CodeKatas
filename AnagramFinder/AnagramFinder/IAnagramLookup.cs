4ce65d8f46dd9b0a59f625d5a09428376e3e5496
Tim VanFosson <tvanfosson@gmail.com>
2013-12-14 12:33:35 -0600

    Add interface for the algorithms that will do the work of finding anagrams

---
+++ AnagramFinder/AnagramFinder/IAnagramLookup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AnagramFinder
+{
+    public interface IAnagramLookup
+    {
+        /// <summary>
+        /// Given a word and a list of words, find all the words in the list
+        /// that are anagrams of the given word.
+        /// </summary>
+        /// <param name="word">The starting word.</param>
+        /// <param name="wordList">A list of candidate words.</param>
+        /// <returns>A list of words from the word list, including the start word, that are anagrams of the start word.</returns>
+        IEnumerable<string> FindAnagrams(string word, IEnumerable<string> wordList);
+    }
+}
